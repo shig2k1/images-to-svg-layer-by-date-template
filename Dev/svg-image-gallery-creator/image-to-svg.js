55c1980b5c2eadaec789c660820df312877af9cd
= <=>
2021-09-01 20:58:33 +0100

    reorganised files

--- Dev/svg-image-gallery-creator/image-to-svg.js
+++ Dev/svg-image-gallery-creator/image-to-svg.js
@@ -13,24 +13,21 @@ const SVG_TEMPLATE = `<?xml version="1.0" encoding="utf-8"?>
 
       // hide all the layers
       for (let i=1; i<=31; i++) {
-          //document.getElementById('_' + i).style.display = 'none'
+          document.getElementById('_' + i).style.display = 'none'
       }
 
       // get the number of todays date - so if it's the 2nd of april, that'd be 2
       let date = new Date().getDate()
 
-      // if you want to test a particular layer, change the date below to a value between 1 - 31 & remove the // prefix
-      //date = 30
-
       // make the right layer visible
-      // document.getElementById('_' + date).style.display = 'block'
+      document.getElementById('_' + date).style.display = 'block'
       console.log('hello!')
     ]]>
   </script>
   </svg>
 `
 
-const BG = `<image style="overflow:visible;enable-background:new    ;" width="1000" height="1000" id="/*ID*/" href="data:image/png;base64,/*HREF*/" />`
+const BG = `<image id="BG" style="overflow:visible;enable-background:new    ;" width="1000" height="1000" href="data:image/png;base64,/*HREF*/" />`
 const IMG = `<image style="overflow:visible;enable-background:new    ;" width="1000" height="883" id="/*ID*/" href="data:image/png;base64,/*HREF*/" />`
 
 async function readDirAndConvert () {
@@ -42,16 +39,19 @@ async function readDirAndConvert () {
       
       // the file names were odd - quicker for me to rename them here
       const transformFileName = (name) => name.split('_')[2]
+      const fileNameToNum = (name) => parseInt(transformFileName(name).split('.')[0])
       // sort the files with bg first
-      const reorganisedFiles = files.sort((a, b) => transformFileName(a) > transformFileName(b) ? 1 : -1)
-                                    .filter(name => transformFileName(name))
+      const reorganisedFiles = files
+        .filter(name => transformFileName(name))
+        .sort((a, b) => fileNameToNum(a) > fileNameToNum(b) ? 1 : -1)
+                                    
                               
       const bg64 = await imageToBase64(path.resolve(dir, reorganisedFiles.shift()))
       let imgSvg = BG.replace('/*HREF*/', bg64)
       
       for (let file of reorganisedFiles) {
         const base64 = await imageToBase64(path.resolve(dir, file))
-        imgSvg += IMG.replace('/*ID*/', file.split('.')[0]).replace('/*HREF*/', base64)
+        imgSvg += IMG.replace('/*ID*/', `${transformFileName(file).split('.')[0]}`).replace('/*HREF*/', base64)
       }
 
       const html = SVG_TEMPLATE.replace('/*CODE*/', imgSvg)
