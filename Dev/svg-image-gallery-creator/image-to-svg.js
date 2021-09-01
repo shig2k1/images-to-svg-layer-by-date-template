387383aeac2be7eab2ae46aa0a39643b7765b630
= <=>
2021-09-01 21:03:15 +0100

    changed generation

--- Dev/svg-image-gallery-creator/image-to-svg.js
+++ Dev/svg-image-gallery-creator/image-to-svg.js
@@ -5,14 +5,15 @@ const imageToBase64 = require('image-to-base64');
 
 const SVG_TEMPLATE = `<?xml version="1.0" encoding="utf-8"?>
   <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 883" style="enable-background:new 0 0 1000 883;" xml:space="preserve">
+    <g>
     /*CODE*/
-
+    </g>
     <script  type="text/javascript">
     <![CDATA[
       const $bg = document.getElementById('BG')
 
       // hide all the layers
-      for (let i=1; i<=31; i++) {
+      for (let i=1; i<=1; i++) {
           document.getElementById(i).style.display = 'none'
       }
 
